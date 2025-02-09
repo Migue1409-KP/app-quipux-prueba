033a79200ed694ecd3d67dc52742eb24a0a53ff4
Miguel Mosquera <mopri38@gmail.com>
2025-02-08 19:33:45 -0500

    Agregar configuración de rutas y dependencias en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {Router, RouterModule, RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pruebatecnicafront';
+export class AppComponent implements OnInit {
+  
+  constructor(private router: Router) {}
+  
+  ngOnInit() {
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CreateTrackListComponent } from './pages/create-tracklist/create-tracklist.component';
+import { FindAllTrackListsComponent } from './pages/find-all-tracklists/find-all-tracklists.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  { 
+    path: 'create-tracklist',
+    component: CreateTrackListComponent
+  },
+  {
+    path: 'find-all-tracklists',
+    component: FindAllTrackListsComponent
+  }
+];
